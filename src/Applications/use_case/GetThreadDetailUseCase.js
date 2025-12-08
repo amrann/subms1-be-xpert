9cfecc9b815baca047d0c24a39a33da40f656968
mraaan•🍉 <amr.bin.an@gmail.com>
2025-12-08 13:55:14 +0700

    fix: fixing issue clean architecture and tableTestHelper implementation

--- src/Applications/use_case/GetThreadDetailUseCase.js
+++ src/Applications/use_case/GetThreadDetailUseCase.js
@@ -5,7 +5,37 @@ class GetThreadDetailUseCase {
 
   async execute(threadId) {
     await this._threadRepository.checkThreadExist(threadId);
-    return this._threadRepository.getDetailThread(threadId);
+
+    const { thread, comments, replies, likes } = await this._threadRepository.getDetailThread(threadId);
+
+    const likeCounts = {};
+    likes.forEach(like => {
+      likeCounts[like.comment_id] = Number(like.like_count);
+    });
+
+    const mappedThread = {
+      id: thread.id,
+      title: thread.title,
+      body: thread.body,
+      date: thread.date,
+      username: thread.username,
+      comments: comments.map(comment => ({
+        id: comment.id,
+        username: comment.username,
+        date: comment.date,
+        content: comment.is_delete ? '**komentar telah dihapus**' : comment.content,
+        likeCount: likeCounts[comment.id] || 0,
+        replies: replies
+          .filter(reply => reply.comment_id === comment.id)
+          .map(reply => ({
+            id: reply.id,
+            content: reply.is_deleted ? '**balasan telah dihapus**' : reply.content,
+            date: reply.date,
+            username: reply.username
+          }))
+      }))
+    };
+    return mappedThread;
   }
 }
 
--- src/Interfaces/http/api/threads/handler.js
+++ src/Interfaces/http/api/threads/handler.js
@@ -27,40 +27,11 @@ class ThreadsHandler {
   async getThreadDetailHandler(request, h) {
     const getThreadDetailUseCase = this._container.getInstance(GetThreadDetailUseCase.name);
     const { threadId } = request.params;
-    const { thread, comments, replies, likes } = await getThreadDetailUseCase.execute(threadId);
-
-    const likeCounts = {};
-    likes.forEach(like => {
-      likeCounts[like.comment_id] = Number(like.like_count);
-    });
-
-    const mappedThread = {
-      id: thread.id,
-      title: thread.title,
-      body: thread.body,
-      date: thread.date,
-      username: thread.username,
-      comments: comments.map(comment => ({
-        id: comment.id,
-        username: comment.username,
-        date: comment.date,
-        content: comment.is_delete ? '**komentar telah dihapus**' : comment.content,
-        likeCount: likeCounts[comment.id] || 0,
-        replies: replies
-          .filter(reply => reply.comment_id === comment.id)
-          .map(reply => ({
-            id: reply.id,
-            content: reply.is_deleted ? '**balasan telah dihapus**' : reply.content,
-            date: reply.date,
-            username: reply.username
-          }))
-      }))
-    };
-
+    const thread = await getThreadDetailUseCase.execute(threadId);
     const response = h.response({
       status: 'success',
       data: {
-        thread: mappedThread
+        thread
       }
     });
     response.code(200);
